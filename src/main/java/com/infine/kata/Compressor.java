6e6633f8d9e39702d94e93f62a7c857d79a940f9
antoine <aramponi@infine.com>
2018-09-26 17:52:29 +0200

    added Compressor

---
+++ src/main/java/com/infine/kata/Compressor.java
@@ -0,0 +1,11 @@
+package com.infine.kata;
+
+public class Compressor {
+    public static String compress(String in) {
+        return "";
+    }
+
+    public static String uncompress(String in) {
+        return "";
+    }
+}
