573586b779a852c855961094444c4c3820b16ab8
Antoine Ramponi <antoine.ramponi@gmail.com>
2018-10-15 16:42:21 +0200

    split test function

--- src/test/java/com/infine/kata/CompressorTest.java
+++ src/test/java/com/infine/kata/CompressorTest.java
@@ -5,12 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CompressorTest {
-    @Test(expected=IllegalArgumentException.class)
+
     public void should_compress_a_given_string(){
         assertEquals("3A2B5A3B9C5A", Compressor.compress("AAABBAAAAABBBCCCCCCCCCAAAAA"));
         assertEquals("3A2B5A3B9C55", Compressor.compress("AAABBAAAAABBBCCCCCCCCC55555"));
         assertEquals("1A1B1C1D1E1F1G1H1I1J1K1L", Compressor.compress("ABCDEFGHIJKL"));
         assertEquals("", Compressor.compress(""));
+
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void should_throw_an_exception_when_null_parameter() {
         Compressor.compress(null);
     }
 
